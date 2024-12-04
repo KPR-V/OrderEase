e37020b1d5bbf52fd90ac85f2d26391867207169
VARUN KAPOOR <148445736+KPR-V@users.noreply.github.com>
2024-12-04 14:34:44 +0530

    Update connectdb.js

--- frontend/components/connectdb.js
+++ frontend/components/connectdb.js
@@ -2,12 +2,18 @@ import mongoose from 'mongoose';
 
 export const connectdb = async () => {
   try {
-    if (mongoose.connections && mongoose.connections[0].readyState) {
+    if (mongoose.connection.readyState >= 1) {
       console.log('Already connected to database');
       return;
     }
 
-    const { connection } = await mongoose.connect(process.env.MONGODB_URI, { dbName: "OrderEase" });
+    mongoose.set('strictQuery', false);
+
+    const { connection } = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "OrderEase"
+    });
     console.log(`Connected to database: ${connection.host}`);
   } catch (error) {
     console.error('Error connecting to database:', error.message);
